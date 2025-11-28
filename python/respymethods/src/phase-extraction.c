a7739e250391251bdbe73198cee9448939806918
ElioBalestrieri <elio.balestrieri@gmail.com>
2025-11-28 18:35:05 +0100

    numpy-C API: two_point_interp

--- python/respymethods/src/phase-extraction.c
+++ python/respymethods/src/phase-extraction.c
@@ -3,7 +3,7 @@
 #include <numpy/arrayobject.h>
 
 
-static PyObject *times2array(PyObject *self, PyObject *args) {
+static PyObject *two_point_interp(PyObject *self, PyObject *args) {
 
 	PyArrayObject* array;
     
@@ -15,30 +15,86 @@ static PyObject *times2array(PyObject *self, PyObject *args) {
     int ndim = PyArray_NDIM(array);
     npy_intp* dims = PyArray_DIMS(array);
     int dtype = PyArray_TYPE(array);
-	npy_intp size = PyArray_SIZE(array);
+	npy_intp arr_len = PyArray_SIZE(array);
+	int typenum_int = NPY_INT;
 
     double *datain = (double*)PyArray_DATA(array);
 
+	// allocate an array for events for loc min and max (null derivative)
+    PyObject *events_arr = PyArray_SimpleNew(ndim, dims, typenum_int);
+	int *null_der = (int*)PyArray_DATA((PyArrayObject*)events_arr);
 
-    // Create new array: PyArray_SimpleNew(ndim, dims, typenum)
-    PyObject *result = PyArray_SimpleNew(ndim, dims, dtype);
-    double *dataout = (double*)PyArray_DATA((PyArrayObject*)result);
+    // Allocate the resulting array of phase values
+    PyObject *phase_arr = PyArray_SimpleNew(ndim, dims, dtype);
+    double *phase_tmp = (double*)PyArray_DATA((PyArrayObject*)phase_arr);
 
-
-	for (npy_intp i=0; i<size; i++){
+	// first pass: assign 1 and -1 to peaks and troughs, respectively
+	npy_int acc_ = 0;
+	npy_int peakhere = 0;
+	npy_int troughhere = 0;
+	null_der[0] = 0;
+	
+	for (npy_int j=1; j<arr_len; j++){
 		
-		dataout[i] = datain[i]*2.0;		
+		peakhere = ((datain[j]>datain[j+1]) & (datain[j]>datain[j-1]));	
+		troughhere = -1*((datain[j]<datain[j+1]) & (datain[j]<datain[j-1]));
+		null_der[j] = peakhere + troughhere;
+		acc_ = acc_ + peakhere - troughhere;
+
 	}
 
-	return result;
-	
+	// allocate an array to store indexes of events and calculate in this way the interval
+	npy_intp dims_ev[1] = {acc_};
+	 
+    PyObject *idxs_arr = PyArray_SimpleNew(ndim, dims_ev, typenum_int);
+	int *idx_der = (int*)PyArray_DATA((PyArrayObject*)idxs_arr);
+
+	// second pass: store the indexes of events (peaks and troughs)
+	acc_ = 0;
+	for (npy_int j=0; j<arr_len; j++){
+		if (null_der[j]){
+			idx_der[acc_] = j;
+			acc_ ++;				
+		}		
+	}
+
+
+	// last pass: interpolate phase
+	npy_double this_entry = NAN;
+	npy_int acc_event = 0;
+	npy_int npoints = 0;
+	npy_double acc_phase = NAN;
+
+	phase_tmp[0] = NAN;
+	for (npy_int j=1; j<arr_len; j++){
+		if (null_der[j]){
+			this_entry = M_PI*(1-null_der[j])/(2);			
+			if (acc_event<acc_){
+				npoints = idx_der[acc_event+1]-idx_der[acc_event];
+				acc_phase = (M_PI/(double)npoints); 							
+				acc_event ++;				
+			}
+			else {
+				acc_phase = NAN;
+			}			
+		}
+		if (null_der[j-1]==-1){
+			this_entry = -M_PI;			
+		}
+		else {
+			this_entry = this_entry+acc_phase;			
+		}
+		phase_tmp[j] = this_entry;		
+	}
+
+	return phase_arr;	
+
 }
 
 
-
 // Method definitions
-static PyMethodDef cpytest_methods[] = {
-	{"times2array", times2array, METH_VARARGS, "multiply times 2 each element of an array"},
+static PyMethodDef phase_extraction_methods[] = {
+	{"two_point_interp", two_point_interp, METH_VARARGS, "extract instantaneous phase via 2 point interpolation"},
 	{NULL, NULL, 0, NULL}
 };
 
@@ -46,9 +102,9 @@ static PyMethodDef cpytest_methods[] = {
 static struct PyModuleDef PhaseExtraction = {
 	PyModuleDef_HEAD_INIT,
 	"PhaseExtraction", // __name__
-	"Hello", // __doc__
-	32, // m_size, set to -1 meaning that the module does not support sub-interpreters. https://docs.python.org/3/c-api/module.html#c.PyModuleDef
-	cpytest_methods
+	"", // __doc__
+	32, // m_size  https://docs.python.org/3/c-api/module.html#c.PyModuleDef
+	phase_extraction_methods
 };
 
 // Module inititalization
