10d0e36668f90abd9e681e011f7fbfd54d715382
ElioBalestrieri <elio.balestrieri@gmail.com>
2025-11-28 11:06:10 +0100

    first sketch clustering in python

--- python/sketchpad.py
+++ python/sketchpad.py
@@ -18,7 +18,6 @@ from GenerateSurrogates import IAAFT
 
 binhr = loadmat("../_exampledata/binhr.mat")["binhr"].T
 pb = loadmat("../_exampledata/phasebinvect.mat")["pb"]
-prev_shuffled = loadmat("../_exampledata/surrbinhr.mat")["sbinhr"]
 
 # quicktest: scipy fft or numpy fft?
 # result: scipy fft slightly better in both accuracy & speed. 
@@ -54,7 +53,27 @@ if QUICKTEST:
 
 # create surrogates
 
-NPERM = 1000
-indat = detrend(binhr)
+NPERM = 10
+
+# detrend data (I preferred not to bloat the IAAFT with stuff that can be more 
+# easily and intuitively implemented by the user externally)
+indat = detrend(binhr, axis=0)
 
 perm_mat = IAAFT(indat, NPERM)
+
+
+# prelim stats
+nsubjs = indat.shape[1]
+tvals = np.sqrt(nsubjs)*indat.mean(axis=1)/indat.std(axis=1)
+
+# sketch localclust here
+masklength = np.abs(tvals) > 2
+anglevect = pb.copy()
+
+masklength = 1*masklength[:, np.newaxis]
+cmplx_repr = masklength*np.exp(anglevect * 1j)
+
+X, Y = np.meshgrid(cmplx_repr, cmplx_repr)
+dists = np.round(np.abs(X-Y), 5)
+shortlenarc = dists == np.unique(dists)[1]
+
